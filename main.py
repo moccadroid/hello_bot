2aeb555c636e60d68dd854ac682b263e9e8966f4
Maximilian Uhlig <maximilian.uhlig@nuwings.at>
2016-07-27 11:38:38 +0200

    - initial hello_bot commit

---
+++ main.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import json, urllib
+from flask import Flask, request, abort
+from google.appengine.api import urlfetch
+
+app = Flask(__name__)
+
+access_token = '<facebook_access_token>'
+
+
+# webhook for facebook to initialize the bot
+@app.route('/webhook', methods=['GET'])
+def get_webhook():
+
+    if not 'hub.verify_token' in request.args or not 'hub.challenge' in request.args:
+        abort(400)
+
+    return request.args.get('hub.challenge')
+
+
+@app.route('/webhook', methods=['POST'])
+def post_webhook():
+    data = request.json
+
+    if data["object"] == "page":
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                if "message" in messaging_event:
+
+                    sender_id = messaging_event['sender']['id']
+
+                    if 'text' in messaging_event['message']:
+                        message_text = messaging_event['message']['text']
+                        reply(sender_id, message_text)
+
+    return "ok", 200
+
+
+def reply(recipient_id, message_text):
+    params = {
+        "access_token": access_token
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+
+    print data
+
+    url = "https://graph.facebook.com/v2.6/me/messages?" + urllib.urlencode(params)
+    r = urlfetch.fetch(url=url, headers=headers, method='POST', payload=data)
