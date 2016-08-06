9132ef84ee44d3c7445335afc2732ecb8fb88854
Maximilian Uhlig <maximilian.uhlig@nuwings.at>
2016-08-06 13:30:31 +0200

    - heroku tut setup

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 app = Flask(__name__)
 
-access_token = 'facebook-access-token'
+access_token = 'EAAXk6GLIlXcBAHRnz32wf1En9dwNyFBM89KBo6AK38OnMoEmNEGCuNQoGeK4bdl1H5Nq0EA63N95Q7WXjZBoInvkwrGEfDATG9dOZB9Lr2tuHtJbKwGzEppZCus3QWEntls7UZClT7jZC32XGYzl74HQrQubfeJGm6DMhyxqUnAZDZD'
 
 
 @app.route("/", methods=["GET"])
@@ -112,5 +112,3 @@ def create_generic_template_element(title, image_url, subtitle):
         "image_url": image_url,
         "subtitle": subtitle
     }
-
-
