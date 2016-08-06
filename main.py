1afd7d715e11437324e464150ac20acec053b98c
Maximilian Uhlig <maximilian.uhlig@nuwings.at>
2016-08-06 12:09:48 +0200

    - heroku tut setup

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def post_webhook():
                         message_text = messaging_event['message']['text']
                         image = "http://cdn.shopify.com/s/files/1/0080/8372/products/tattly_jen_mussari_hello_script_web_design_01_grande.jpg"
                         element = create_generic_template_element("Hello", image, message_text)
-                        reply_with_generic_template(sender_id, element)
+                        reply_with_generic_template(sender_id, [element])
 
                         #do_rules(sender_id, message_text)
 
